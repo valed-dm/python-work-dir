5a5fecc598b63e73dad66e913ac065192a812200
valed-dm <dmvaled@gmail.com>
2022-10-27 18:28:24 +0300

    Some useful exceptions

--- natural_input_check.py
+++ natural_input_check.py
@@ -25,3 +25,23 @@ while True:
         print('Something strange has happened here... Sorry!')
         data_entered()
         continue
+
+# TypeError
+# short_list = [1]
+# one_value = short_list[0.5]
+
+# AttributeError
+short_list = [1]
+short_list.append(2)
+short_list.depend(3)
+
+# SyntaxError
+# This exception is raised when the control reaches a line of code which violates Python's grammar. 
+# It may sound strange, but some errors of this kind cannot be identified without first running the code. 
+# This kind of behavior is typical of interpreted languages – the interpreter always works in a hurry 
+# and has no time to scan the whole source code. 
+# It is content with checking the code which is currently being run. 
+# An example of such a category of issues will be presented very soon.
+
+# It's a bad idea to handle this exception in your programs. 
+# You should produce code that is free of syntax errors, instead of masking the faults you’ve caused.
