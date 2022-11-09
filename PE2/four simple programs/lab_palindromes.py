953f9e25531014f3a2f2b0d34222b1daa727627a
valed-dm <dmvaled@gmail.com>
2022-11-09 10:16:49 +0300

    lab palindromes completed

---
+++ PE2/four simple programs/lab_palindromes.py
@@ -0,0 +1,19 @@
+# palindrome checkered
+
+str = input("Please enter your string: ")
+str = str.replace(" ", "").upper()
+crit = False
+
+for i in range(len(str)):
+    if str[i] == str[len(str) - i - 1]:
+        crit = True
+        continue
+    crit = False
+    if len(str) > 0:
+        print("str length = ", len(str), "steps made = ", i + 1)
+    break
+
+if len(str) == 0 or not crit:
+    print("It's not a palindrome")
+else:
+    print("It's a palindrome")
