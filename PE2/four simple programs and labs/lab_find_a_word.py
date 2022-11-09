37031da5995d2b93b1b86aa265f60b94cc29767f
valed-dm <dmvaled@gmail.com>
2022-11-09 15:47:36 +0300

    lab find a word completed

---
+++ PE2/four simple programs and labs/lab_find_a_word.py
@@ -0,0 +1,22 @@
+# find a word
+
+count = 0
+cycle = 0
+
+word = input("please, input word: ")
+str = input("please, input string: ")
+
+for ch in word:
+    cycle += 1
+    if str.find(ch) != -1:
+        count += 1
+    else:
+        break
+
+print("count", count)
+print("cycle", cycle)
+
+if len(word) == count:
+    print("Yes!")
+else:
+    print("No!")
