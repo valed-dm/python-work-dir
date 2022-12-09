7d40cc39ec28bcf1abd3dcceb16ff32414237b44
valed-dm <dmvaled@gmail.com>
2022-12-10 00:17:07 +0300

    calendar weekday func

---
+++ PE2/intro CALENDAR module/weekday_func.py
@@ -0,0 +1,17 @@
+# The weekday() function
+# Another useful function provided by the calendar module is the function called weekday, 
+# which returns the day of the week as an integer value for the given year, month, and day. 
+# Let's see it in practice.
+
+# Run the code in the editor to check the day of the week that falls on December 24, 2020.
+
+import calendar
+print(calendar.weekday(2022, 12, 10))
+
+# Result:
+
+# 3
+# output
+
+# The weekday function returns 3, which means that December 24, 2020 is a Thursday.
+
