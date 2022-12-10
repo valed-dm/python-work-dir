cd05e88a041d363121dab03cb7dfc7dbb8507581
valed-dm <dmvaled@gmail.com>
2022-12-10 08:57:05 +0300

    classes for creating calendars

---
+++ PE2/intro CALENDAR module/creating CALENDAR obj/classes_for_calendar_creation.py
@@ -0,0 +1,24 @@
+# Classes for creating calendars
+
+# The presented functions aren't everything the calendar module offers.
+
+# In addition to them, we can use the following classes:
+
+# calendar.Calendar – provides methods to prepare calendar data for formatting;
+
+# calendar.TextCalendar – is used to create regular text calendars;
+
+# calendar.HTMLCalendar – is used to create HTML calendars;
+
+# calendar.LocalTextCalendar – is a subclass of the calendar.TextCalendar class.
+# The constructor of this class takes the locale parameter,
+# which is used to return the appropriate months and weekday names.
+
+# calendar.LocalHTMLCalendar – is a subclass of the calendar.HTMLCalendar class.
+# The constructor of this class takes the locale parameter,
+# which is used to return the appropriate months and weekday names.
+
+
+# During this course, you've already had the opportunity to create text calendars when discussing the functions of the calendar module.
+
+# Time to try something new. Let's take a closer look at the methods of the calendar class.
